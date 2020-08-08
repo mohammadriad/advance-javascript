d75777a323f3918f55393f326d5509d51d3add02
mohammadriad <riadahmmed289@gmail.com>
2020-08-09 00:17:08 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,28 @@
+// Falsy:
+
+// false
+//  0
+//  ""
+//  undifind
+// null
+// Nan
+
+// Truethy  :
+
+//   "0", " "
+// [],{}
+// "false"
+
+
+
+
+
+ 
+let name2 =5;
+ 
+if(name2 || name2 == 0){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
