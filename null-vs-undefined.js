c5474f285218dd17d57d89df39be038937fa323e
mohammadriad <riadahmmed289@gmail.com>
2020-08-10 16:32:00 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,18 @@
+let pakhi;
+
+
+
+
+ 
+
+function premik()
+
+const premik = {name: "mujnu", phone: 01245612};
+console.log(premik.name);
+ 
+let fun = undefined;
+console.log(fun);
+
+
+let ages = [2, 5, 9];
+console.log(ages[11]);
